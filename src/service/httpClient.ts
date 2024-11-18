a1bb090eb500f2ee8690920e46f4230430724b65
felipe <dev.felipemignone@gmail.com>
2024-11-18 20:25:19 -0300

    feat: add http client handler

---
+++ src/service/httpClient.ts
@@ -0,0 +1,49 @@
+export default class HttpClient {
+  #baseUrl: string;
+
+  constructor(baseUrl?: string) {
+    this.#baseUrl = baseUrl || (process.env.BASE_API_URL as string);
+  }
+
+  get = (path: string, query?: string) =>
+    fetch(`${this.#baseUrl}/${path}${query && "/" + query}`, {
+      method: "GET",
+      credentials: "include",
+    });
+
+  post = (path: string, body: unknown, query?: string) =>
+    fetch(`${this.#baseUrl}/${path}${query && "/" + query}`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+  put = (path: string, body: unknown, query?: string) =>
+    fetch(`${this.#baseUrl}/${path}${query && "/" + query}`, {
+      method: "PUT",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+  patch = (path: string, body: unknown, query?: string) =>
+    fetch(`${this.#baseUrl}/${path}${query && "/" + query}`, {
+      method: "PATCH",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    });
+
+  delete = (path: string, query?: string) =>
+    fetch(`${this.#baseUrl}/${path}${query && "/" + query}`, {
+      method: "DELETE",
+      credentials: "include",
+    });
+}
